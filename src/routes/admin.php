45f54cf92c91861b17e7f4ff4ac027246d5a39b2
KaijiOoyama <ookj.0312@gmail.com>
2022-07-11 22:49:16 +0900

    リソースコントローラ

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 /*
@@ -73,4 +74,6 @@ Route::middleware('auth:admin')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+
+    Route::resource('owners', OwnersController::class);
 });
