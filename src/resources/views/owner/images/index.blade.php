72059b1a9f45ae4bf183bcbd52a7c96a640baa89
KaijiOoyama <ookj.0312@gmail.com>
2022-07-16 17:41:42 +0900

    image create validation

--- src/resources/views/owner/images/index.blade.php
+++ src/resources/views/owner/images/index.blade.php
@@ -15,7 +15,7 @@
                             新規登録する
                         </button>
                     </div>
-                    @foreach ($imagess as $image)
+                    @foreach ($images as $image)
                     <div class="w-1/4 p-4">
                         <a href="{{ route('owner.images.edit', ['image' => $image->id])}}">
                             <div class="border rounded-md p-4">
